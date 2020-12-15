888feee8475f28b46b5f21119d27d021dbb39918
Marko Kungla <marko.kungla@gmail.com>
2020-12-15 15:13:06 +0200

    Run go tests as subbenchmarks

--- examples/go/fib_test.go
+++ examples/go/fib_test.go
@@ -4,14 +4,16 @@ import (
 	"testing"
 )
 
-func BenchmarkFib10(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		var _ = Fib(10)
-	}
-}
+func BenchmarkFib(b *testing.B) {
+	b.Run("Fib10", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			var _ = Fib(10)
+		}
+	})
+	b.Run("Fib20", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			var _ = Fib(20)
+		}
+	})
 
-func BenchmarkFib20(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		var _ = Fib(20)
-	}
 }
